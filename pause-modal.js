2ae263ff6cf04f57e3aa8b9486218cde98eff6ed
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-04-27 20:09:27 +0300

    Created a modal that opens when the pause button is clicked, and added functions to close it when clicked outside or when a key is pressed

---
+++ pause-modal.js
@@ -0,0 +1,37 @@
+const pauseModal = document.querySelector(".pause-modal");
+const pauseButton = document.querySelector(".pause-button");
+const closeButton = document.querySelector(".close-button");
+const continueButton = document.querySelector(".continue-button");
+
+const closePauseModal = function () {
+  pauseModal.classList.toggle("hidden");
+  pauseModal.close();
+};
+
+// Opening modal when Pause button is clicked
+pauseButton.addEventListener("click", function () {
+  pauseModal.classList.remove("hidden");
+  pauseModal.showModal();
+});
+
+// Closing modal when "X" button is clicked
+closeButton.addEventListener("click", function () {
+  closePauseModal();
+});
+
+// Closing modal when "ESC" key is clicked
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !pauseModal.classList.contains("hidden"))
+    closePauseModal();
+});
+
+// Closing modal when the outside of the modal is clicked
+document.addEventListener("click", function (e) {
+  if (e.target === pauseModal) closePauseModal();
+});
+
+// Going back to the game when the "Continue button" is clicked
+
+continueButton.addEventListener('click', function () {
+    closePauseModal();
+}) 
