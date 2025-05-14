e43023f365cf38d0a5cfa0127d46bc8be76d8d5a
davosab <davidsabau72@gmail.com>
2025-05-14 21:49:45 +0300

    getPossibleMoves() for knight

--- Knight.js
+++ Knight.js
@@ -1,10 +1,39 @@
 class Knight extends Piece {
-  constructor(colour) {
-    super();
+  moves = [ // changes to the index of the initial position
+    [-1, -2], 
+    [-2, -1], 
+    [-2, 1], 
+    [-1, 2], 
+    [1, 2], 
+    [2, 1], 
+    [2, -1], 
+    [1, -2]
+  ];
+  constructor(colour, row, col) {
+    super(row, col);
     this.colour = colour;
     this.icon = (colour == "white") ? "♘" : "♞" ;
   }
+
   getIcon() {
     return this.icon;
+  }
+
+  getPossibleMoves() {
+    let possibleMoves = [];
+    for (const move of moves) {
+      let possibleMove = [this.row + move[0], this.col + move[1]];
+      if (
+        possibleMove[0] > 7 || possibleMove[0] < 0 || // row is on board
+        possibleMove[1] > 7 || possibleMove[1] < 0 // col is on board   
+      ) continue;
+
+      // if piece on square is of same colour, skip
+      if (board[possibleMove[0], possibleMove[1]].colour == this.colour) 
+        continue;
+
+      possibleMoves.push([this.row + move[0], this.col + move[1]]);
+    }
+    return possibleMoves;
   }
 }
--- Piece.js
+++ Piece.js
@@ -1,14 +1,18 @@
 class Piece {
-    colourSquares(arrayPossibleSquares){
-        for(let possibleMoves of arrayPossibleSquares) {
-            let row = possibleMoves.row;
-            let col = possibleMoves.col;
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+  colourSquares(arrayPossibleSquares) {
+    for (let possibleMoves of arrayPossibleSquares) {
+      let row = possibleMoves.row;
+      let col = possibleMoves.col;
 
-            if(row >= 0 && row <= 7 && col >= 0 && col <= 7){
-                fill("#fbd275");
-                rect(col * s, row * s, s, s); 
-            }
-        }
-    }   
+      if (row >= 0 && row <= 7 && col >= 0 && col <= 7) {
+        fill("#fbd275");
+        rect(col * s, row * s, s, s);
+      }
+    }
+  }
 
 }
