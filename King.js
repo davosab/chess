439c4fdedfbe8b1dcf048b8c48a65e0bc6ddadd1
davosab <davidsabau72@gmail.com>
2025-05-15 15:57:54 +0300

    moved getPossibleMoves() to Piece (for knight, king & pawn only)

--- King.js
+++ King.js
@@ -21,23 +21,23 @@ class King extends Piece {
     return this.#icon;
   }
 
-  getPossibleMoves() {
-    let possibleMoves = [];
-    for (const move of this.moves) {
-      let possibleMove = [this.row + move[0], this.col + move[1]];
-      if (
-        possibleMove[0] > 7 || possibleMove[0] < 0 || // row is on board
-        possibleMove[1] > 7 || possibleMove[1] < 0 // col is on board   
-      ) continue;
+  // getPossibleMoves() {
+  //   let possibleMoves = [];
+  //   for (const move of this.moves) {
+  //     let possibleMove = [this.row + move[0], this.col + move[1]];
+  //     if (
+  //       possibleMove[0] > 7 || possibleMove[0] < 0 || // row is on board
+  //       possibleMove[1] > 7 || possibleMove[1] < 0 // col is on board   
+  //     ) continue;
 
-      // if piece on square is of same colour, skip
-      if (
-        board[possibleMove[0]][possibleMove[1]] != null &&
-        board[possibleMove[0]][possibleMove[1]].colour == this.colour
-      ) continue;
+  //     // if piece on square is of same colour, skip
+  //     if (
+  //       board[possibleMove[0]][possibleMove[1]] != null &&
+  //       board[possibleMove[0]][possibleMove[1]].colour == this.colour
+  //     ) continue;
 
-      possibleMoves.push([this.row + move[0], this.col + move[1]]);
-    }
-    return possibleMoves;
-  }
+  //     possibleMoves.push([this.row + move[0], this.col + move[1]]);
+  //   }
+  //   return possibleMoves;
+  // }
 }
--- Pawn.js
+++ Pawn.js
@@ -21,26 +21,26 @@ class Pawn extends Piece {
     return this.#icon;
   }
 
-  getPossibleMoves() {
-    let possibleMoves = [];
+  // getPossibleMoves() {
+  //   let possibleMoves = [];
 
-    // if (!this.#hasMoved) this.moves.pop(); // remove option to move 2 squares
+  //   // if (!this.#hasMoved) this.moves.pop(); // remove option to move 2 squares
     
-    for (const move of this.moves) {
-      let possibleMove = [this.row + move[0], this.col + move[1]];
-      if (
-        possibleMove[0] > 7 || possibleMove[0] < 0 || // row is on board
-        possibleMove[1] > 7 || possibleMove[1] < 0 // col is on board   
-      ) continue;
+  //   for (const move of this.moves) {
+  //     let possibleMove = [this.row + move[0], this.col + move[1]];
+  //     if (
+  //       possibleMove[0] > 7 || possibleMove[0] < 0 || // row is on board
+  //       possibleMove[1] > 7 || possibleMove[1] < 0 // col is on board   
+  //     ) continue;
 
-      // if piece on square is of same colour, skip
-      if (
-        board[possibleMove[0]][possibleMove[1]] != null &&
-        board[possibleMove[0]][possibleMove[1]].colour == this.colour
-      ) continue;
+  //     // if piece on square is of same colour, skip
+  //     if (
+  //       board[possibleMove[0]][possibleMove[1]] != null &&
+  //       board[possibleMove[0]][possibleMove[1]].colour == this.colour
+  //     ) continue;
 
-      possibleMoves.push([this.row + move[0], this.col + move[1]]);
-    }
-    return possibleMoves;
-  }
+  //     possibleMoves.push([this.row + move[0], this.col + move[1]]);
+  //   }
+  //   return possibleMoves;
+  // }
 }
