c52caadd67b135bd208892350c9e6ac73e0329f9
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-05-16 15:57:51 +0300

    Modified the piece classes so that can show moves

--- Bishop.js
+++ Bishop.js
@@ -1,4 +1,38 @@
 class Bishop extends Piece {
+  moves = [
+   // Up-right
+    [-1, 1],
+    [-2, 2],
+    [-3, 3],
+    [-4, 4],
+    [-5, 5],
+    [-6, 6],
+    [-7, 7],
+    // Up-left
+    [-1, -1],
+    [-2, -2],
+    [-3, -3],
+    [-4, -4],
+    [-5, -5],
+    [-6, -6],
+    [-7, -7],
+    // Down-right
+    [1, -1],
+    [2, -2],
+    [3, -3],
+    [4, -4],
+    [5, -5],
+    [6, -6],
+    [7, -7],
+    // Down-left
+    [1, 1],
+    [2, 2],
+    [3, 3],
+    [4, 4],
+    [5, 5],
+    [6, 6],
+    [7, 7],
+  ]
   constructor(colour, row, col) {
     super(colour, row, col);
     this.icon = colour === "w" ? "♗" : "♝";
@@ -7,37 +41,39 @@ class Bishop extends Piece {
   getIcon() {
     return this.icon;
   }
-getPossibleMoves(row, col, board) {
-  const bishopPossibleMoves = [];
-
-  // Up-Right
-  for (let r = row - 1, c = col + 1; r >= 0 && c < 8; r--, c++) {
-    if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
-  else break;
-  }
-
-  // Up-Left
-  for (let r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
-    if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
-     else break;
-    
-  }
-
-  // Down-Right
-  for (let r = row + 1, c = col + 1; r < 8 && c < 8; r++, c++) {
-    if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
-    else break;
-  }
-  // Diagonală jos-stânga
-  for (let r = row + 1, c = col - 1; r < 8 && c >= 0; r++, c--) {
-    if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
-       else break;
-  }
-
-  return bishopPossibleMoves;
 }
+
+// getPossibleMoves(row, col, board) {
+//   const bishopPossibleMoves = [];
+
+//   // Up-Right
+//   for (let r = row - 1, c = col + 1; r >= 0 && c < 8; r--, c++) {
+//     if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
+//   else break;
+//   }
+
+//   // Up-Left
+//   for (let r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
+//     if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
+//      else break;
+    
+//   }
+
+//   // Down-Right
+//   for (let r = row + 1, c = col + 1; r < 8 && c < 8; r++, c++) {
+//     if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
+//     else break;
+//   }
+//   // Diagonală jos-stânga
+//   for (let r = row + 1, c = col - 1; r < 8 && c >= 0; r++, c--) {
+//     if (board[r][c] === null) bishopPossibleMoves.push({ row: r, col: c });
+//        else break;
+//   }
+
+//   return bishopPossibleMoves;
+// }
 
   
-}
+// }
 
 
--- Queen.js
+++ Queen.js
@@ -2,20 +2,24 @@ class Queen extends Piece {
   constructor(colour, row, col) {
     super(colour, row, col);
     this.icon = colour === "white" ? "♕" : "♛";
-  }
+    const rook = new Rook();
+    const bishop = new Bishop();
+
+    this.moves = [...rook.moves, ...bishop.moves]
+ }
 
   getIcon() {
     return this.icon;
   }
-
-  getPossibleMoves(row, col, board) {
-    const rookMoves = new Rook(this.colour).getPossibleMoves(row, col, board);
-    const bishopMoves = new Bishop(this.colour).getPossibleMoves(
-      row,
-      col,
-      board
-    );
-
-    return rookMoves.concat(bishopMoves);
-  }
 }
+//   getPossibleMoves(row, col, board) {
+//     const rookMoves = new Rook(this.colour).getPossibleMoves(row, col, board);
+//     const bishopMoves = new Bishop(this.colour).getPossibleMoves(
+//       row,
+//       col,
+//       board
+//     );
+
+//     return rookMoves.concat(bishopMoves);
+//   }
+// }
