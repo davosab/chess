abfe5d0bc768646c7108947a645e7e4e9fb27e9d
davosab <davidsabau72@gmail.com>
2025-05-26 13:29:54 +0300

    commit before pull, trying to not allow a piece to move out of the way of a check

--- Piece.js
+++ Piece.js
@@ -17,7 +17,7 @@ class Piece {
 
   getPossibleMoves() {
     let possibleMoves = [];
-    // let nr = 0;
+    // let clonedBoard = board.map(row => row.slice());
 
     // add diagonal take if pawn
     if (this instanceof Pawn) this.checkForDiagonalTake();
@@ -65,7 +65,6 @@ class Piece {
       }
     }
 
-    // console.log(possibleMoves);
     return possibleMoves;
   }
 
@@ -130,11 +129,9 @@ class Piece {
         }
       }
     }
-    const possibleMoves = this.getPossibleMoves();
-    let checks = possibleMoves.some(move =>
+    return this.getPossibleMoves().some(move =>
       move.row == kingRow && move.col == kingCol
     );
-    return checks;
   }
 
 }
