5251a04408a417e3c208e2c275b772cb33c82c18
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-05-27 19:00:50 +0300

    Finished Offer a draw modals

--- draw-game-modal.js
+++ draw-game-modal.js
@@ -1,5 +1,7 @@
 const playAgainYesButton = document.querySelector('.play-again-yes-button');
 const playAgainNoButton = document.querySelector('.play-again-no-button');
+const settingsModal = document.querySelector("[data-settings-modal]");
+const finishedGameModal = document.querySelector(".finished-game-modal");
 
 const closeGameDrawModal = function() {
     drawGameModal.classList.toggle("hidden");
@@ -8,5 +10,14 @@ const closeGameDrawModal = function() {
 
 playAgainYesButton.addEventListener('click', function() {
     closeGameDrawModal();
+    settingsModal.classList.remove("hidden");
+    settingsModal.classList.add("flex");
+    settingsModal.showModal();
  });
+
+ playAgainNoButton.addEventListener('click', function() {
+    closeGameDrawModal();
+    finishedGameModal.classList.remove('hidden');
+    finishedGameModal.showModal();
+ })
 
